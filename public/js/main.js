9181563b91a82921d758a47f294b3cf15e2f0a99
Shakhawat <shsourov2000@gmail.com>
2022-04-23 01:59:37 +0600

    change categories section design

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     // Popular Courses 
     // New Courses
     $('#popular-courses, #new-courses').slick({
@@ -27,6 +27,41 @@ $(document).ready(function () {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    // Categories
+    $('#category-courses').slick({
+        dots: false,
+        loop: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
                     slidesToScroll: 1
                 }
             }
